8e0fa07a111c512a987cb2bd1c0052aa3ab0abec
lenaStsk <staskevichalena@gmail.com>
2023-09-01 23:01:49 +0200

    Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { Layout, Menu, Image, Typography, Space } from 'antd'
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Main from './pages/main/Main'
+import logoIcom from './assets/hacker-news.svg'
+import './style.css'
+
+const { Header, Content } = Layout
+const { Link, Text } = Typography
 
 function App (): JSX.Element {
   return (
-    <Main />
+      <BrowserRouter>
+        <Header style={{ display: 'flex', alignItems: 'center' }}>
+          <Link href="/" target="_blank">
+            <Space>
+              <Image src={logoIcom} width={40} preview={false}></Image>
+              <Text>The Hacker News</Text>
+            </Space>
+          </Link>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} />
+        </Header>
+        <Content>
+          <Routes>
+            <Route path="/" element={<Main/>}/>
+          </Routes>
+        </Content>
+      </BrowserRouter>
   )
 }
 
